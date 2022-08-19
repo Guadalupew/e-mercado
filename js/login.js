e446f9412e4b493f2ec1d6263e387e8d12c42a7e
Guadalupew <wetzelguadalupe@gmail.com>
2022-08-19 01:07:12 -0300

    Modificación Login
    Se realizó el Login en html y sus funcionalidades en javascript

--- js/login.js
+++ js/login.js
@@ -1,1 +1,15 @@
-a
+let nombre = document.getElementById('usuario');
+let contrasenia = document.getElementById('contrasenia');
+let error = document.getElementById('error');
+error.style.color = 'red';
+
+function ingresar(){
+    
+    if (usuario.value === '' || contrasenia.value === '') {
+      error.innerHTML = 'Ingresa tu usuario y contraseña.';
+    } else {
+    window.location.href = "login.html" 
+    }
+   
+}
+
